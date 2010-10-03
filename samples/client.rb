40bd0f52f1d65146878b9c3504d8d22011216eb4
Sho Hashimoto <hashimoto@shokai.org>
2010-10-03 13:33:15 +0900

    use eventmachine in sample

--- samples/client.rb
+++ samples/client.rb
@@ -3,31 +3,27 @@
 require 'rubygems'
 require 'socket'
 require 'json'
+require 'eventmachine'
 $KCODE = 'u'
 
 HOST = "192.168.1.37"
 PORT = 20000
 
 s = TCPSocket.open(HOST, PORT)
-s.puts "MESSAGE shokaishokai hellohellohello"
+s.puts "MESSAGE shokaishokai sample/client.rb start"
 
-msgs = Array.new
-loop do
-  Thread.start(s){|s|
-    loop do
-      begin
-        res = s.gets
-        exit unless res
-        p JSON.parse res
-      rescue => e
-        STDERR.puts e
-      end
-      sleep 0.1
-    end
-  }
-  Thread.start(s){|s|
-    loop do
-      s.puts gets
-    end
-  }
+EventMachine::run do
+  loop do
+    res = s.gets
+    exit unless res
+    res = JSON.parse res rescue next
+    p res
+    sleep 0.1
+  end
+end
+
+EventMachine::run do
+  loop do
+    s.puts gets
+  end
 end
--- samples/zanmai_bot.rb
+++ samples/zanmai_bot.rb
@@ -5,6 +5,7 @@
 require 'rubygems'
 require 'socket'
 require 'json'
+require 'eventmachine'
 $KCODE = 'u'
 
 HOST = "192.168.1.37"
@@ -13,25 +14,22 @@ PORT = 20000
 s = TCPSocket.open(HOST, PORT)
 s.puts "MESSAGE shokaishokai ざんまいbot start"
 
-msgs = Array.new
-loop do
-  Thread.start(s){|s|
-    loop do
-      begin
-        res = s.gets
-        exit unless res
-        p res = JSON.parse(res)
-        if res['type'] == 'chat_message' 
-          if res['body'] =~ /ざんまい/ # キーワードに反応
-            s.puts "CHATMESSAGE #{res['chat']} ざんまい行きたい！"
-          elsif res['body'] =~ /かず(すけ|助)/
-            s.puts "CHATMESSAGE #{res['chat']} かずにゃんぺろぺろ"
-          end
+EventMachine::run do
+  loop do
+    begin
+      res = s.gets
+      exit unless res
+      p res = JSON.parse(res)
+      if res['type'] == 'chat_message' 
+        if res['body'] =~ /ざんまい/ # キーワードに反応
+          s.puts "CHATMESSAGE #{res['chat']} ざんまい行きたい！"
+        elsif res['body'] =~ /かず(すけ|助)/
+          s.puts "CHATMESSAGE #{res['chat']} かずにゃんぺろぺろ"
         end
-      rescue => e
-        STDERR.puts e
       end
-      sleep 0.1
+    rescue => e
+      STDERR.puts e
     end
-  }
+    sleep 0.1
+  end
 end
